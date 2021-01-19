d2aecdaacf1035bb90232fdd664844bb32cb2cbd
NOTEBOOK-XPS\mathi <mathiasgam@hotmail.com>
2021-01-19 17:27:30 +0100

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    systemversion "latest"
+    cppdialect "C++17"
+    staticruntime "On"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+
+    filter "configurations:Release"
+		optimize "on"
+		runtime "Release"
+
+
+  	filter "configurations:Debug"
+		symbols "on"
+		runtime "Debug"
